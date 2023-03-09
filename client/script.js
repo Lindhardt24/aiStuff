4ed2280481f8941ea1f32ea6f159a18f0097f3f5
Lindhardt24 <Lindhardt1964@gmail.com>
2023-03-09 21:03:43 +0100

    update the server url/

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('https://aistuff.onrender.com', { 
+  const response = await fetch('https://aistuff.onrender.com/', { 
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
